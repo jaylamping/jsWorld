9dde7964869c1e701085f58fb1976ec397f03872
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-12-17 15:03:17 -0600

    envelope refactor

--- js/primitives/Envelope.js
+++ js/primitives/Envelope.js
@@ -1,35 +1,31 @@
+import { Polygon } from './';
 import { angle, subtract, translate } from '../math/utils';
-import { Polygon } from './index';
 
 export class Envelope {
-  constructor(skeleton, width, roundness = 20) {
+  constructor(skeleton, width, roundness = 1) {
     this.skeleton = skeleton;
-    this.roundness = roundness;
     this.poly = this.#generatePolygon(width, roundness);
   }
 
-  draw(ctx) {
-    this.poly.draw(ctx);
+  draw(ctx, options) {
+    this.poly.draw(ctx, options);
   }
 
   #generatePolygon(width, roundness) {
     const { p1, p2 } = this.skeleton;
+
     const radius = width / 2;
     const alpha = angle(subtract(p1, p2));
-    const alphaClockwise = alpha + Math.PI / 2;
-    const alphaCounterClockwise = alpha - Math.PI / 2;
-    // const p1Clockwise = translate(p1, alphaClockwise, radius);
-    // const p2Clockwise = translate(p2, alphaClockwise, radius);
-    // const p1CounterClockwise = translate(p1, alphaCounterClockwise, radius);
-    // const p2CounterClockwise = translate(p2, alphaCounterClockwise, radius);
+    const alpha_cw = alpha + Math.PI / 2;
+    const alpha_ccw = alpha - Math.PI / 2;
+
     const points = [];
     const step = Math.PI / Math.max(1, roundness);
-    // adding epsilon to avoid artifacts
     const eps = step / 2;
-    for (let i = alphaCounterClockwise; i <= alphaClockwise + eps; i += step) {
+    for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
       points.push(translate(p1, i, radius));
     }
-    for (let i = alphaCounterClockwise; i <= alphaClockwise + eps; i += step) {
+    for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
       points.push(translate(p2, Math.PI + i, radius));
     }
 
